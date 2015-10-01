d72bd1abbe56aeeaf9def3341cbbdd30848461c6
Heather Gibling <heathergibling@gmail.com>
2015-10-01 15:03:21 -0400

    added sample data and loop to run aSPUpath on all gene sets

--- aSPUpath and HYST.R
+++ aSPUpath and HYST.R
@@ -27,7 +27,7 @@ unique.genes <- unique(gene.list)
 
 mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
 
-gene.info <- getBM(attributes=c("entrezgene", "chromosome_name", "start_position", "end_position"), filters=c("chromosome_name", "entrezgene"), values=list(chromosome_name=c(1:22, "X", "Y"), entrezgene=c(unique.genes)), mart=mart)
+all.gene.info <- getBM(attributes=c("entrezgene", "chromosome_name", "start_position", "end_position"), filters=c("chromosome_name", "entrezgene"), values=list(chromosome_name=c(1:22, "X", "Y"), entrezgene=c(unique.genes)), mart=mart)
 
 ## 108 genes are not in this list--are not mapped to a chromosome in biomart
 ## some are microRNAs, others unlocalized scaffolds
@@ -38,7 +38,44 @@ gene.info <- getBM(attributes=c("entrezgene", "chromosome_name", "start_position
 
 genes.in.gs <- function(gene.set, master.list) {
 	set.genes <- as.numeric(master.list[[gene.set]])
-	master.position <- gene.info[,1] %in% set.genes
-	return(gene.info[master.position,])
+	master.position <- all.gene.info[,1] %in% set.genes
+	return(all.gene.info[master.position,])
 }
+
+
+# load sample dataset
+
+assoc.data <- read.table("~/Desktop/gwas.assoc", header=T)
+permuted.pvals <- read.csv("~/Desktop/perm_pvalues.txt")
+
+
+# extract snp info
+
+snp.info <- data.frame(SNP=assoc.data[,2], Chrom=assoc.data[,1], Position=assoc.data[,3])
+
+
+### Run aSPUpath for all gene sets
+
+aSPUpath.results <- data.frame(Pathway=NA, Pval=NA)
+
+for (i in 1:length(gs.kegg)) {
+	gene.info <- genes.in.gs(i, gs.kegg)
+	ldmatrix <- 
+	results <- aSPUsPath(assoc.data$P, 	# P values of SNPs
+		corrSNP=ldmatrix, 				# correlation of SNPs to controls
+		pow=c(1, 2, 4, 8, Inf), 		# SNP gamma values
+		pow2=c(1, 2, 4, 8), 			# gene gamma values
+		snp.info=snp.info, 				# SNP location info
+		gene.info=gene.info, 			# gene location info
+		n.perm=1000, 					# 1000 permutations
+		Ps=T)							# using P values instead of Z scores
+	aSPUpath.results[i,1] <- names(gs.kegg[1])
+	aSPUpath.results[i,2] <- results[21]
+}
+
+
+# order pathways/gene sets by significance
+
+aSPUpath.sig <- aSPUpath.results[order(aSPUpath.results$Pval)]
+
 
