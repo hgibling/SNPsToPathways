71be7a01f5e621761aff332aff62ce25e33beda4
Heather Gibling <heathergibling@gmail.com>
2015-10-06 10:59:29 -0400

    fixed function for getting gene info; made function for running aSPUpath or HYST for any of the three gene set collections

--- aSPUpath and HYST.R
+++ aSPUpath and HYST.R
@@ -20,48 +20,65 @@ assoc.data <- read.table("~/Desktop/gwas.assoc", header=T)
 	# Correlation data
 
 
-# Generate KEGG gene sets
+### Generate gene set collections
+
+# KEGG
 
 get.kegg.gs <- kegg.gsets(species="hsa", id.type="entrez")
-kegg.gs <- get.gs.kegg$kg.sets
+kegg.gs <- get.kegg.gs$kg.sets
 
 
-# Generate GO Biological Pathways gene sets
-
+# GO Biological Pathways
 gobp.gs <- annFUN.org("BP", mapping="org.Hs.eg.db", ID="entrez")
 
 
-# Import predefined gene sets
+# Import predefined gene sets from Bader Lab
 
-bader.gs <- GSA.read.gmt("~/Desktop/Human_AllPathways_January_28_2015_symbol.gmt")
+get.bader.gs <- GSA.read.gmt("~/Desktop/Human_AllPathways_January_28_2015_symbol.gmt")
 
 
-# Generate master list of genes in KEGG or GO BP gene sets
+# Remove blank entry at end of each Bader Lab gene set
+
+for (i in 1:length(get.bader.gs$genesets)) {
+	get.bader.gs$genesets[[i]] <- get.bader.gs$genesets[[i]][-length(get.bader.gs	$genesets[[i]])]
+}
+
+bader.gs <- get.bader.gs$genesets
+names(bader.gs) <- get.bader.gs$geneset.names
+
+
+### Generate master list of genes in each gene set collection
 
 get.master.list <- function(gene.set.list) {
 	gene.list <- c()
 	for (i in 1:length(gene.set.list)) {
-		genes <- as.numeric(gene.set.list[[i]])
+		genes <- gene.set.list[[i]]
 		gene.list <- c(gene.list, genes)
 		}
 	unique.genes <- unique(gene.list)
 	return(unique.genes)
 }
 
-kegg.master <- get.master.list(kegg.gs)
-gobp.master <- get.master.list(gobp.gs)
+kegg.master <- as.numeric(get.master.list(kegg.gs))
+gobp.master <- as.numeric(get.master.list(gobp.gs))
+bader.master <- get.master.list(bader.gs)	# gene symbols, so not numeric
 
 
-# Get chromosome locations for the KEGG or GO BP master list of genes
+# Get chromosome locations for the master list of genes for each collection
 
 mart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
 
-get.gene.info <- function (master.list) {
-	all.gene.info <- getBM(attributes=c("entrezgene", "chromosome_name", 	"start_position", "end_position"), filters=c("chromosome_name", 	"entrezgene"), values=list(chromosome_name=c(1:22, "X", "Y"), 				entrezgene=c(master.list)), mart=mart)
+get.gene.info <- function (master.list, type="ID") {
+	if (type=="symbol") {
+		all.gene.info <- getBM(attributes=c("hgnc_symbol", "chromosome_name", 		"start_position", "end_position"), filters=c("chromosome_name", 		"hgnc_symbol"), values=list(chromosome_name=c(1:22, "X", "Y"), 					hgnc_symbol=c(master.list)), mart=mart)
+	} else {
+		all.gene.info <- getBM(attributes=c("entrezgene", "chromosome_name", 		"start_position", "end_position"), filters=c("chromosome_name", 		"entrezgene"), values=list(chromosome_name=c(1:22, "X", "Y"), 					entrezgene=c(master.list)), mart=mart)
+	}
 }
 
 kegg.gene.info <- get.gene.info(kegg.master)
 gobp.gene.info <- get.gene.info(gobp.master)
+bader.gene.info <- get.gene.info(bader.master, type="symbol")
 
 ## some genes are not in these lists--are not mapped to a chromosome in biomart
 ## some are microRNAs, others unlocalized scaffolds
@@ -71,98 +88,98 @@ gobp.gene.info <- get.gene.info(gobp.master)
 # Extract gene info for one gene set
 
 genes.in.gs <- function(gene.set.position, gene.set.list, master.gene.info) {
-	set.genes <- as.numeric(gene.set.list[[gene.set.position]])
+	set.genes <- gene.set.list[[gene.set.position]]
 	master.position <- master.gene.info[,1] %in% set.genes
 	return(master.gene.info[master.position,])
 }
 
 
-# Extract SNP info from dataset
+### Extract SNP info from dataset
 
 snp.info <- data.frame(SNP=assoc.data[,2], Chrom=assoc.data[,1], Position=assoc.data[,3])
 
 
-# Generate linkage disequilibrium matrix (SNP correlation matrix)
+### Generate linkage disequilibrium matrix (SNP correlation matrix)
 
-ld.matrix <-
+control.data <- read.table("~/Desktop/GO_Quad_DATA-clean-CEU.traw", stringsAsFactors=F, header=T)
+
+rownames(control.data) <- control.data$SNP
+
+
+# Remove uneccesary columns
+
+control.genotypes <- control.data[,-(1:6)]
+
+
+# Transpose so columns are SNPs
+
+control.trans <- t(control.genotypes)
+
+ld.matrix <- cor(control.trans)
 
 
 ### Run aSPUpath
 
-# For KEGG gene sets
-
-aSPUpath.kegg <- data.frame(Pathway=NA, Pval=NA)
-
-for (i in 1:length(kegg.gs)) {
-	gene.info <- genes.in.gs(i, kegg.gs, kegg.gene.info)
-	results.kegg.a <- aSPUsPath(assoc.data$P, 	# P values of SNPs
-		corrSNP=ld.matrix, 				# correlation of SNPs to controls
-		pow=c(1, 2, 4, 8, Inf), 		# SNP gamma values
-		pow2=c(1, 2, 4, 8), 			# gene gamma values
-		snp.info=snp.info, 				# SNP location info
-		gene.info=gene.info, 			# gene location info
-		n.perm=1000, 					# 1000 permutations
-		Ps=T)							# using P values instead of Z scores
-	aSPUpath.kegg[i,1] <- names(kegg.gs[i])
-	aSPUpath.kegg[i,2] <- results.kegg.a[21]
+run.snp.gsa <- function(collection, method) {
+	results.df <- data.frame(Pathway=NA, Pval=NA)
+	if (collection=="kegg") {
+		gs <- kegg.gs
+		all.gene.info <- kegg.gene.info
+	} else if (collection=="gobp") {
+		gs <- gobp.gs
+		all.gene.info <- gobp.gene.info
+	} else if (collection=="bader") {
+		gs <- bader.gs
+		all.gene.info <- bader.gene.info
+	} 
+	if (method=="aSPUpath") {
+		for (in in 1:length(gs)) {
+			gene.info <- genes.in.gs(i, gs, all.gene.info)
+			results <- aSPUsPath(assoc.data$P, 	# P values of SNPs
+				corrSNP=ld.matrix,				# correlation of SNPs to controls
+				pow=c(1, 2, 4, 8, Inf),			# SNP gamma values
+				pow2=c(1, 2, 4, 8),				# gene gamma values
+				snp.info=snp.info,				# SNP location info
+				gene.info=gene.info,			# gene location info
+				n.perm=1000,					# 1000 permutations
+				Ps=T)							# P values instead of Z scores
+			results.df[i,1] <- names(gs[i])
+			results.df[i,2] <- results[21]
+		}
+	} else if (method=="HYST") {
+		for (i in 1:length(gs)) {
+			gene.info <- genes.in.gs(i, gs, all.gene.info)
+			results <- Hyst(assoc.data$P,
+				ldmatrix=ld.matrix,
+				snp.info=snp.info,
+				gene.info=gene.info)
+			results.df[i,1] <- names(gs[i])
+			results.df[i,2] <- results[21]
+		}
+	}
+	return(results.df)
 }
 
-aSPUpath.kegg.sig <- aSPUpath.kegg[order(aSPUpath.kegg$Pval)]
+
+# Run aSPUpath and order results by significance
+
+kegg.aSPUpath <- run.snp.gsa(collection="kegg", method="aSPUpath")
+kegg.aSPUpath.sig <- kegg.aSPUpath[order(kegg.aSPUpath$Pval)]
+
+gobp.aSPUpath <- run.snp.gsa(collection="gobp", method="aSPUpath")
+gobp.aSPUpath.sig <- gobp.aSPUpath[order(gobp.aSPUpath$Pval)]
+
+bader.aSPUpath <- run.snp.gsa(collection="bader", method="aSPUpath")
+bader.aSPUpath.sig <- bader.aSPUpath[order(bader.aSPUpath$Pval)]
 
 
-# For GO BP gene sets
+# Run HYST and order results by significance
 
-aSPUpath.gobp <- data.frame(Pathway=NA, Pval=NA)
+kegg.hyst <- run.snp.gsa(collection="kegg", method="HYST")
+kegg.hyst.sig <- kegg.hyst[order(kegg.hyst$Pval)]
 
-for (i in 1:length(gobp.gs)) {
-	gene.info <- genes.in.gs(i, gobp.gs, gobp.gene.info)
-	results.gobp.a <- aSPUsPath(assoc.data$P, 	# P values of SNPs
-		corrSNP=ld.matrix, 				# correlation of SNPs to controls
-		pow=c(1, 2, 4, 8, Inf), 		# SNP gamma values
-		pow2=c(1, 2, 4, 8), 			# gene gamma values
-		snp.info=snp.info, 				# SNP location info
-		gene.info=gene.info, 			# gene location info
-		n.perm=1000, 					# 1000 permutations
-		Ps=T)							# using P values instead of Z scores
-	aSPUpath.gobp[i,1] <- names(gobp.gs[i])
-	aSPUpath.gobp[i,2] <- results.gobp.a[21]
-}
+gobp.hyst <- run.snp.gsa(collection="gobp", method="HYST")
+gobp.hyst.sig <- gobp.hyst[order(gobp.hyst$Pval)]
 
-aSPUpath.gobp.sig <- aSPUpath.gobp[order(aSPUpath.gobp$Pval)]
-
-
-### Run HYST
-
-# For KEGG gene sets
-
-hyst.kegg <- data.frame(Pathway=NA, Pval=NA)
-
-for (i in 1:length(kegg.gs)) {
-	gene.info <- genes.in.gs(i, kegg.gs, kegg.gene.info)
-	results.kegg.h <- Hyst(assoc.data$P, 	# P values of SNPs
-		ldmatrix=ld.matrix, 			# correlation of SNPs to controls
-		snp.info=snp.info, 				# SNP location info
-		gene.info=gene.info) 			# gene location info
-	hyst.kegg[i,1] <- names(kegg.gs[i])
-	hyst.kegg[i,2] <- results.kegg.h[21]
-}
-
-hyst.kegg.sig <- hyst.kegg[order(hyst.kegg$Pval)]
-
-
-# For GO BP gene sets
-
-hyst.gobp <- data.frame(Pathway=NA, Pval=NA)
-
-for (i in 1:length(gobp.gs)) {
-	gene.info <- genes.in.gs(i, gobp.gs, gobp.gene.info)
-	results.gobp.h <- Hyst(assoc.data$P, 	# P values of SNPs
-		ldmatrix=ld.matrix, 			# correlation of SNPs to controls
-		snp.info=snp.info, 				# SNP location info
-		gene.info=gene.info) 			# gene location info
-	hyst.gobp[i,1] <- names(gobp.gs[i])
-	hyst.gobp[i,2] <- results.gobp.h[21]
-}
-
-hyst.gobp.sig <- hyst.gobp[order(hyst.gobp$Pval)]
-
+bader.hyst <- run.snp.gsa(collection="bader", method="HYST")
+bader.hyst.sig <- bader.hyst[order(bader.hyst$Pval)]
