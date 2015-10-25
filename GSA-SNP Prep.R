10b9e90cfb78f12ace74c23dbbde57ddb365d411
Heather Gibling <heathergibling@gmail.com>
2015-10-25 14:49:38 -0400

    made code generic for use in any working directory

--- GSA-SNP Prep.R
+++ GSA-SNP Prep.R
@@ -2,17 +2,19 @@
 
 ### Load sample dataset
 
-assoc.data <- read.table("~/Desktop/gwas.assoc", header=T)
-permuted.pvals <- read.csv("~/Desktop/perm_pvalues.txt")
+assoc.data <- read.table("gwas.assoc", header=T)
+permuted.pvals <- read.csv("perm_pvalues.txt")
 
 
 ### Combine SNP IDs with P values
 
 gsasnp.input <- data.frame(SNP=assoc.data$SNP, P=assoc.data$P, permuted.pvals[,-1], P.100=permuted.pvals[,1])
 
-# first permuted column renamed 'P.100' so avoid having two columns named 'P'
+# first permuted column renamed 'P.100' to avoid having two columns named 'P'
 
 
 ### Print file
 
-write.table(gsasnp.input, "~/Desktop/GSA-SNP Input.txt", col.names=T, row.names=F, sep="\t", quote=F)
+write.table(gsasnp.input, "GSA-SNP Input.txt", col.names=T, row.names=F, sep="\t", quote=F)
+
+# file can now be uploaded in GSA-SNP software for analysis
